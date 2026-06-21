9bfaabb9279271a76e42c370db19e170a74440cb
koralissa <54867710+koralissa@users.noreply.github.com>
2026-06-21 09:14:44 -0400

    UI Update - Fixed bug with persistent header not displaying on all pages

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'vivea-v2';
+const CACHE = 'vivea-v3';
 const PRECACHE = ['./index.html', './style.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', e => {
